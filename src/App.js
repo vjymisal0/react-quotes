a6014369f5e379500f38cc73ca7a507f6afc6fb9
Vijay Misal <misalvijay153@gmail.com>
2024-04-11 23:01:08 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ class App extends React.Component {
   state = {
     advice: ''
   };
-  componentDidMount() {
-    this.fetchAdvice();
-  }
+  // componentDidMount() {
+  //   this.fetchAdvice();
+  // }
   fetchAdvice = () => {
     axios.get('https://api.adviceslip.com/advice').then((response) => {
       const { advice } = response.data.slip
@@ -22,9 +22,10 @@ class App extends React.Component {
       <div className='app'>
         <div className='card'>
           <h1 className='heading'>{advice}</h1>
-          <button className='button'>
+          <button onClick={this.fetchAdvice} className='button'>
             <span>GIVE ME ADVICE!</span>
           </button>
+          
         </div>
       </div>
     )
